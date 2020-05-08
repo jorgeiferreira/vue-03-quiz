36920a880b779e42d21b4743d0442e86f97441ef
Jorge Ferreira <jferreira@intellisys.com.do>
2020-05-08 17:43:09 -0400

    adding test data

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    quizList: [
+      {
+        id:1,
+        tags: "js, vuejs, vuex",
+        title: "VueJs Basic Quiz",
+        description: "Test your vue js knowledge", 
+        minScore:80,
+        questions: [
+        { id: 1, text: "What is vuejs?", type:"single", options:["C# library", "js library", "movie", "place"], answers: [1] },
+        { id: 2, text: "What is key in vuejs?", type:"multi", options:["option 1", "option 2", "option 3", "option 4"], answer: [2,3] }
+      ]
+    }]
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters:{
+    getQuizList:(state)=>{
+      state.quizList
+    },
+    getQuizById: (state)=>(id)=>{
+      return state.quizList.filter(quiz=>quiz.id===id)
+    }
   }
 })
